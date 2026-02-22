ae161e3369ab08c50ec53b48664da994a4f93c98
user <user@DESKTOP-8NT95FK>
2026-02-22 22:28:41 +0530

    changes

---
+++ TNS_Javacode/src/oopsdemo/MethodOverriding.java
@@ -0,0 +1,33 @@
+package oopsdemo;
+
+class Payment{
+	void makepayment() {
+		System.out.println("Processing payment using generic method");
+	}
+}
+
+class UpiPayment extends Payment{
+	void makepayment() {
+		System.out.println("Payment done using UPI");
+	}
+	
+}
+
+class CardPayment extends Payment{
+	void makepayment() {
+		System.out.println("Payment done using Card");
+	}
+}
+public class MethodOverriding {
+
+	public static void main(String[] args) {
+		
+		Payment payment;
+		payment = new UpiPayment();
+		payment.makepayment();
+		
+		payment = new CardPayment();
+		payment.makepayment();
+	}
+
+}
